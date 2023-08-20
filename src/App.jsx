dad0a39ac4f3331570b9fbd8dfa7706cf2c6c248
BombMarley <kile146@gmail.com>
2023-08-20 20:46:08 +0200

    Fetching orders

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Error from "./ui/Error";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
-import Order from "./features/order/Order";
+import Order, { loader as orderLoader } from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
 
 const router = createBrowserRouter([
@@ -36,7 +36,12 @@ const router = createBrowserRouter([
         path: "/order",
         element: <Order />,
       },
-      { path: "/order/:orderId", element: <Order /> },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
+      },
     ],
   },
 ]);
