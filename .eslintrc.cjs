89e5b2abb8ea5298378ad46336ae06ced59d1019
BombMarley <kile146@gmail.com>
2023-08-20 17:35:20 +0200

    Fetching pizza menu with react router Loaders

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,10 +13,10 @@ module.exports = {
   settings: { react: { version: "18.2" } },
   plugins: ["react-refresh"],
   rules: {
+    "react/proptypes": "off",
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
     ],
-    "react/proptypes": "off",
   },
 };
