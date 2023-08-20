c2762a6075cbae31113af9af7b8dabed4e554f1e
BombMarley <kile146@gmail.com>
2023-08-20 17:50:17 +0200

    Turn off react prop-types

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,7 +13,7 @@ module.exports = {
   settings: { react: { version: "18.2" } },
   plugins: ["react-refresh"],
   rules: {
-    "react/proptypes": "off",
+    "react/prop-types": "off",
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
